4334ee89b0e49027bc023f9522c3b8271def232e
knifoon <knifoon@gmail.com>
2025-11-03 07:41:30 -0800

    Create ebaypricecharting.user.js

---
+++ ebaypricecharting.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         PriceCharting Link
+// @namespace    http://knifoon.com
+// @version      1
+// @description  Adds a link to pricecharting page if available
+// @author       knifoon
+// @match        https://www.ebay.com/itm/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ebay.com
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(async function() {
+    function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+
+            // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log('loaded script')
+        waitForElm('.x-item-title').then(title => {
+            let item = title.querySelector('h1').innerText
+            item = item.toLowerCase().replace(/(bgs|psa|cgc|ags)(\s)?\d{1,2}(\.5)?/,'')
+            const wordsToRemove = ["tcg","pokemon","card","pokémon","(nm)","(lp)","(mp)","mp","lp","nm","1999","kids","wb","first","movie","wotc","1st","bgs","cgc","psa",'xy','evolutions'];
+            const filteredString = item
+            .split(' ')
+            .filter(word => !wordsToRemove.includes(word.toLowerCase()))
+            .join(' ')
+            console.log(filteredString);
+
+            const extract = filteredString.match(/(.*?)\s#?((?:\w*)\d+)/)
+            if (extract){
+                let name = extract[1].split(' ').join('+')
+                let number = extract[2]
+                let promo = item.toLowerCase().includes('promo') ? '+promo':''
+                title.insertAdjacentHTML('afterend',`<a href="https://www.pricecharting.com/search-products?q=${name}+${number+promo}&type=prices" target="_blank"><img src="https://www.pricecharting.com/images/logo-pricecharting-new.png" width=80px></a>`)
+            }
+        })
+    })
+})();
